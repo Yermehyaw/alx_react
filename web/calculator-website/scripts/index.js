6e80269982a905ddc8fad079b77fa30218de9d4d
Yermehyaw <yermehyaw@gmail.com>
2025-10-13 00:44:21 +0100

    [Update]: Add username displey interactivity

--- web/calculator-website/scripts/index.js
+++ web/calculator-website/scripts/index.js
@@ -2,8 +2,8 @@
 
 
 // Notice for interactive calculator ongoing build
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
+const ctaButton = document.querySelector(".call-to-action");
+ctaButton.addEventListener("click", () => {
     alert("We're Sorry, the Calculator is still being built. Please check back later...");
     });
 
@@ -16,4 +16,32 @@ calcImg.onclick = () => {
     } else {
 	calcImg.setAttribute("src", "./assets/images/modern-calculator-3d.jpg");
     }
+};
+
+
+// Add user specific welcome message
+let nameButton = document.querySelector("header>button");
+let heading = document.querySelector("h1");
+
+function setUserName() {
+    const userName = prompt("Please enter your name");
+    // Should in case the user cancels the prompt
+    if (!userName) {
+	heading.textContent = "Calculator Pro is for You!";
+	return;
+    }
+    localStorage.setItem("name", userName);
+    heading.textContent = `Calculator Pro is for You, ${userName}`;
+    return;
+}
+
+if (!localStorage.getItem("name")) {  // The user's name hasn't been stored?
+    setUserName();
+} else {  // Prevent constant user name prompting when page is refreshed
+    const storedName = localStorage.getItem("name");
+    heading.textContent = `Calculator Pro is for You, ${storedName}`;
+}
+
+nameButton.onclick = () => {
+    setUserName();
 };
