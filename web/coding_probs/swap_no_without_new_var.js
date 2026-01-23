f3a7b69c8a65329f11b1a8ff28a871b975ff32e2
Yermehyaw <yermehyaw@gmail.com>
2026-01-23 01:09:54 +0100

    [Update]: gitignore and improve swap logic

--- web/coding_probs/swap_no_without_new_var.js
+++ web/coding_probs/swap_no_without_new_var.js
@@ -6,7 +6,6 @@
 
 const prompt = require('prompt-sync')({sigint: true});  // Allow ctrl+c to close prompt
 
-
 let a = prompt("Enter the value for a: ");
 let b = prompt("Enter the value for b: ");
 a = Number(a);
@@ -16,15 +15,17 @@ if (a === NaN || b === NaN) {
     return;
 }
 
-console.log(`a is ${a}, and b is ${b}`);
-
-a = a + b;  // 30
-b = a + b;  // 50
+// Swap logic. if a = 10 and b = 20
+a = a + b;  // 10 + 20 = 30
+b = a + b;  // 30 + 20 = 50
 a = b - a;  // a = 20
 b = (b - a) - a;  // b = 10
 
 console.log("Loading. . .");
 
-setTimeout(() => {
-    console.log(`a is now ${a}, and b is now ${b}`);
+setTimeout(() => { // Creates the illusion of processing...
+  console.log(`a is now ${a}`);
+  setTimeout(() => {
+    console.log(`and b is now ${b}`);
+  }, 5000);
 }, 5000);
